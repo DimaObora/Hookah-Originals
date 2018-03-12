b5dc2bd61b9df9d4634f7531c235d14ea2620447
DimaObora <obora.dima@yandex.ru>
2018-03-13 01:53:41 +0300

    выровнял кнопку по вертикале

--- index.php
+++ index.php
@@ -99,11 +99,11 @@ $settings = get_option('hookah_settings');
             <p class="logo-text">
                 Hookah-Originals</p>
         </div></a>
-    <button class="navbar-toggler align-self-end" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+    <div class="collapse navbar-collapse " id="navbarSupportedContent">
         <ul class="navbar-nav  ">
             <li class="nav-item active">
                 <a class="nav-link" href="#home">Главная <span class="sr-only">(current)</span></a>
